d4eac7a6df4a057b15ed39ed9e373f80c55068a5
Prabjit <prabjitv@gmail.com>
2020-12-24 15:54:12 -0700

    mongoose implemented, connecting to altas db, and graphQL retrieving dummy post I made

--- index.js
+++ index.js
@@ -2,18 +2,33 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const { MONGODB } = require('./config');
+const Post = require('./models/Post');
+const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
-  type Query{
-    sayHi: String
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
   }
-`
+  type Query{
+    getPosts: [Post]
+  }
+`;
+
 const resolvers = {
-    Query: {
-      sayHi: () => 'Hello World!!!'
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
     }
-}
+  }
+};
 
 const server = new ApolloServer({
   typeDefs,
@@ -28,9 +43,4 @@ mongoose
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
-  })
-
-server.listen({ port: 5000})
-  .then(res => {
-    console.log(`Server running at ${res.url}`)
   })
